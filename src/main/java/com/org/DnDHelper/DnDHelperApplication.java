8c68a2f8ea8cb07b55c3efe6db4e02e36e60d855
Dreieven <patryknad@gmail.com>
2023-06-09 13:44:42 +0200

    first commit

---
+++ src/main/java/com/org/DnDHelper/DnDHelperApplication.java
@@ -0,0 +1,13 @@
+package com.org.DnDHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/DnDHelper/DnDHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.DnDHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
